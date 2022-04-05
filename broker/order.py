a7826fbb209bb84a133e308ce44ea5e673b5d49c
Federico Roux <rouxfederico@gmail.com>
2022-04-05 18:10:24 -0300

    wip: order.py

--- broker/order.py
+++ broker/order.py
@@ -1,11 +1,13 @@
 class order:
 
-        
     def __init__(self, symbol, settlement, price, size):
-
         self.symbol = symbol
         self.settlement = settlement
         self.price = price
         self.size = size
 
-    def 
+    def sell(self):
+        return
+
+    def buy(self):
+        return
