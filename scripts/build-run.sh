a0af9130f74dc9fab5f4c1a584b5fca8f97988e5
Federico Roux <rouxfederico@gmail.com>
2022-05-27 00:48:16 -0300

    WIP: style, lint, test

---
+++ scripts/build-run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+function style() {
+    printf "\n===================================\n"
+    printf "Checking code style ...\n"
+    printf "===================================\n\n"
+    pycodestyle --show-source --show-pep8 --format=pylint ../
+    pycodestyle --format=pylint ../
+    return $?
+}
+
+
+lint() {
+    printf "\n===================================\n"
+    printf "Linting source files ...\n"
+    printf "===================================\n\n"
+    pylint ../
+}
+
+test() {
+    printf "\n===================================\n"
+    printf "Running unit tests ...\n"
+    printf "===================================\n\n"
+    pytest ../
+}
+
+
+
+MAIN_FILE=main.py
+
+
+style
+lint
+test
+
+
