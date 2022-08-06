886641cdff6218a92f81cf3bdcd91ff681773621
Federico Roux <rouxfederico@gmail.com>
2022-08-06 20:23:43 -0300

    WIP: constructor with classmethod on Auth

--- src/broker/auth/auth.py
+++ src/broker/auth/auth.py
@@ -13,19 +13,37 @@ import os
 class Auth:
     """ pyhomebroker auth data """
 
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data
 
-        self.bin_path = "../bin"
-        self.auth_file = f'{self.bin_path}/Authfile'
+    @classmethod
+    def from_file(cls, filename):
+        cls.auth_file = filename
+        if cls.read_file() != 0:
+            cls.input_account_data()
 
-        try:
-            os.mkdir(self.bin_path)
-        except OSError:
-            # print(error)
-            pass
+        # @classmethod
+        # def from_dict(cls)
 
-        if self.read_file() != 0:
-            self.input_account_data()
+
+# self.bin_path = "../bin"
+#         self.auth_file = f'{self.bin_path}/Authfile'
+
+        # try:
+        #     os.mkdir(self.bin_path)
+        # except OSError:
+        #     # print(error)
+        #     pass
+
+        
+
+    # def __init__(self, dni, user, password, comitente):
+
+    #     self.dni = dni
+    #     self.usr = user
+    #     self.pwd = password
+    #     self.acc = comitente
+    #     self.save_file()
 
     def input_account_data(self):
         """ user enters data manually """
--- src/broker/auth/test/auth_test.py
+++ src/broker/auth/test/auth_test.py
@@ -5,6 +5,7 @@ from mock import patch
 from broker import auth
 import broker
 
+
 class TestAuth(unittest.TestCase):
 
     # def setUp(self) -> None:
@@ -12,14 +13,14 @@ class TestAuth(unittest.TestCase):
     # def tearDown(self) -> None:
     #     auth.input = input
 
-    def test_constructor(self):
+    def test_constructor_auto(self):
 
         with patch.object(auth.Auth, 'read_file') as mock_read_file:
             mock_read_file.return_value = 0
 
-            test_obj = auth.Auth()
+            test_obj = auth.Auth.from_file("/bin/Authfile")
             # with pytest.raises(Exception) as e_info:
-            assert test_obj.bin_path == "../bin"
+            # assert test_obj.bin_path == "../bin"
             assert test_obj.auth_file == "../bin/Authfile"
             self.assertTrue(mock_read_file.called)
 
@@ -31,17 +32,13 @@ class TestAuth(unittest.TestCase):
                 self.assertTrue(mock_read_file.called)
                 self.assertTrue(mock_input_account_data.called)
 
-
-
-
     def test_input_account_data(self):
         auth.input = lambda: '11223344'
 
 
-
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(TestAuth('test_constructor'))
+    suite.addTest(TestAuth('test_constructor_auto'))
     suite.addTest(TestAuth('test_input_account_data'))
     return suite
 
